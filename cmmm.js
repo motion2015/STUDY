5eb645940eb39555b7bd1cdf78e0f22b8e7c26b5
motion2015 <motion74@naver.com>
2017-10-13 11:08:52 +0900

    Update cmmm.js

--- cmmm.js
+++ cmmm.js
@@ -1,1 +1,9 @@
-ㅍㄹㄹㅇㄹㅓㅏㅓㅏㅏ
+function CommonUnit(){
+   this.fn_IS_APP = function(){
+   var phoneOS ="";
+   var agent = navigator.userAgent;
+     if(agent){
+        if(agent.match('iPhone/i) || agent.match(/iPod/i){}
+     }
+   }
+}
